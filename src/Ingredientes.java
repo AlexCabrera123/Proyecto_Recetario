8e9d74e6b885264bf05281eeee34b0856b851f43
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-03-10 11:37:52 -0600

    Clase ingrediente completa

--- src/Ingredientes.java
+++ src/Ingredientes.java
@@ -2,4 +2,36 @@ public class Ingredientes {
     private String nombre;
     private boolean salado;
     private double cantidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isSalado() {
+        return salado;
+    }
+
+    public void setSalado(boolean salado) {
+        this.salado = salado;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Ingredientes(String nombre, boolean salado, double cantidad) {
+        this.nombre = nombre;
+        this.salado = salado;
+        this.cantidad = cantidad;
+        System.out.println("Ingrediente creado");
+
+    }
 }
