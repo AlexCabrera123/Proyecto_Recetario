618ad6c53c16562746d6a6f0b140108b713d8ff8
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-03-10 11:48:26 -0600

    Clase Receta completa

--- src/Receta.java
+++ src/Receta.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class Receta {
+    private double tiempoPreparacion;
+    private ArrayList<Ingredientes> ingredientes;
+    private ArrayList<String> pasos;
+
+    public Receta(double tiempoPreparacion, ArrayList<Ingredientes> ingredientes, ArrayList<String> pasos) {
+        this.tiempoPreparacion = tiempoPreparacion;
+        this.ingredientes = ingredientes;
+        this.pasos = pasos;
+        System.out.println("Receta creada");
+    }
+
+    public double getTiempoPreparacion() {
+        return tiempoPreparacion;
+    }
+
+    public void setTiempoPreparacion(double tiempoPreparacion) {
+        this.tiempoPreparacion = tiempoPreparacion;
+    }
+
+    public ArrayList<Ingredientes> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(ArrayList<Ingredientes> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public ArrayList<String> getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(ArrayList<String> pasos) {
+        this.pasos = pasos;
+    }
 }
