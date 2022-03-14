192735506b945c5367c190e244024fa77664e167
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-03-14 11:48:52 -0600

    Clase Recetario completa

--- src/Recetario.java
+++ src/Recetario.java
@@ -1,7 +1,36 @@
 import java.util.ArrayList;
 
 public class Recetario {
-    ArrayList<Receta> recetas;
+    private ArrayList<Receta> recetas;
+    private String autor, titulo;
 
+    public ArrayList<Receta> getRecetas() {
+        return recetas;
+    }
 
+    public void setRecetas(ArrayList<Receta> recetas) {
+        this.recetas = recetas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Recetario(ArrayList<Receta> recetas, String autor, String titulo) {
+        this.recetas = recetas;
+        this.autor = autor;
+        this.titulo = titulo;
+    }
 }
